8ee6a94dc59e0343fdf1a5d194ea25b4010c621c
ojs <contact@ollyjsmith.com>
2020-06-04 14:28:41 +0100

    possible to disable anim

--- assets/js/src/modules/GFX.js
+++ assets/js/src/modules/GFX.js
@@ -4,24 +4,48 @@ const GFX = function(){
   this.$nav_logo = document.querySelector('[data-dc-localtarget="#related-matters"] svg' );
   this.$gfx = document.querySelector('.dc-gfx');
   this.timeout = null;
+  this.showDelay = 10000;
+  this.initialHide = 2000;
 }
 
 GFX.prototype = {
-  hide: function(){
+  disableAnimation: function(){
+    this.$gfx.style.animationDuration = '0s';
+    this.$nav_logo.style.animationDuration = '0s';
+  },
+  enableAnimation: function(){
+    this.$gfx.style.animationDuration = '';
+    this.$nav_logo.style.animationDuration = '';
+  },
+  hide: function( immediate ){
+    if( immediate ){
+      this.disableAnimation();
+    }
     this.$gfx.classList.add('hidden');
     this.$gfx.classList.remove('visible');
     this.$nav_logo.classList.remove('hidden');
     this.$nav_logo.classList.add('visible');
     clearTimeout( this.timeout );
     this.timeout = setTimeout( () => {
-      this.show();
-    }, 4000 );
+      this.show( true );
+    }, this.showDelay );
+    if( immediate ){
+      this.enableAnimation();
+    }
   },
-  show: function(){
+  show: function( immediate ){
+    if( immediate ){
+      this.disableAnimation();
+    }
     this.$gfx.classList.remove('hidden');
     this.$gfx.classList.add('visible');
     this.$nav_logo.classList.add('hidden');
     this.$nav_logo.classList.remove('visible');
+    if( immediate ){
+      setTimeout(() => {
+        this.enableAnimation();
+      }, 10 );      
+    }
   },
   _onMove: function(){
     this.hide();
@@ -30,15 +54,15 @@ GFX.prototype = {
     let visibilityAPI = F.visibilityChangeCompat();
     this.timeout = setTimeout( () => {
       this.hide();
-    }, 2500 )
+    }, this.initialHide )
     window.addEventListener('mousemove', () => {
      this._onMove();
     });
     if( visibilityAPI.property ){
-      document.addEventListener( visibilityAPI.eventName, () => {
-        if( document[ visibilityAPI.property ] ){ //is hidden
+      document.addEventListener( visibilityAPI.eventName, () => {        
+        if( document[ visibilityAPI.property ] ){ //is hidden    
           clearTimeout( this.timeout );
-          this.show()
+          this.show( true ) //show, immediately
         } else { /*is visible*/ }
       })
     }
