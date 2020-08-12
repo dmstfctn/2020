a90e489e794ec93a11a8ec8e4c152359b7de8053
ojs <contact@ollyjsmith.com>
2020-08-12 14:01:33 +0100

    add correct branch name to deploy command

--- dev/upload.sh
+++ dev/upload.sh
@@ -4,4 +4,4 @@ node build.js upload
 echo "PUSHING TO GANDI"
 git push gandi live
 echo "PUTTING SITE LIVE"
-ssh 1451555@git.dc2.gpaas.net deploy 2020.demystification.co.git
+ssh 1451555@git.dc2.gpaas.net deploy 2020.demystification.co.git live
