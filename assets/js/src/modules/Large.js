0b97127dd00bc9892134d0c1677c9fe09888e3a8
ojs <contact@ollyjsmith.com>
2021-03-28 22:11:39 +0100

    log popstate info

--- assets/js/src/modules/Large.js
+++ assets/js/src/modules/Large.js
@@ -98,7 +98,10 @@ Large.prototype.setupLoader = function(){
   };
 
   let popstateFunction = ( event ) => {
+    console.log('popstateFunction');    
     const state = history.state;
+    console.log( 'EVENT: ', event );
+    console.log( 'STATE:', state );
     if( state.type === 'menu' ){
       this.historyActive = false;
       this.menus.showMenuById( state.id );
